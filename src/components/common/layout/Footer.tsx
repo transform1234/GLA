29fb37ef2ae10f115f4f96088c6d385ba85c1541
sagar <sagarkoshti1990@gmila.com>
2024-11-10 19:28:39 +0530

    fix: Ui

--- src/components/common/layout/Footer.tsx
+++ src/components/common/layout/Footer.tsx
@@ -83,14 +83,17 @@ const Footer: React.FC<FooterProps> = ({
         borderBottomColor={"footerBorderGray"}
         shadow={"FooterShadow"}
       >
-        {footerMenus?.map((item, index) => (
+        {footerMenus?.map((item: any, index) => (
           <PressableNew
             item={item}
             key={`${item}-${index}`}
             cursor="pointer"
             opacity={selected === index ? 1 : 0.5}
             flex={1}
-            onClick={() => setSelected(0)}
+            onClick={() => {
+              setSelected(0);
+              item?.onClick && item?.onClick();
+            }}
           >
             <Text color={selected === index ? "primary.500" : "gray.900"}>
               <Center>

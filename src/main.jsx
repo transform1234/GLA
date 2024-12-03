e9d333f55398649a4bf3420f69320084ae8aa188
deepali chavhan <deechavhan098@gmail.com>
2024-12-03 11:26:24 +0530

    Task #231428 fix: Added condition to prevent sentry from loading entirely

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css'
-import App from './App.jsx'
+import './index.css';
+import App from './App.jsx';
 import '../i18n.js';
-import * as Sentry from '@sentry/react';
 
-Sentry.init({
-  dsn: import.meta.env.VITE_APP_SENTRY_DSN,
-  integrations: [],
-});
+const sentryDsn = import.meta.env.VITE_APP_SENTRY_DSN;
+
+if (sentryDsn) {
+  import('@sentry/react').then(Sentry => {
+    Sentry.init({
+      dsn: sentryDsn,
+      integrations: [],
+    });
+  });
+}
 
 const container = document.getElementById('root');
 if (!container) throw new Error("Root container not found.");

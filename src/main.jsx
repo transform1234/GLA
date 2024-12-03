d98d25f345cef6025ea71b8d554fa75dbd8921de
deepali chavhan <deechavhan098@gmail.com>
2024-12-03 11:04:03 +0530

    Task #231428 fix: Added Sentry in app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import './index.css'
 import App from './App.jsx'
 import '../i18n.js';
+import * as Sentry from '@sentry/react';
 
-createRoot(document.getElementById('root')).render(
+Sentry.init({
+  dsn: import.meta.env.VITE_APP_SENTRY_DSN,
+  integrations: [],
+});
+
+const container = document.getElementById('root');
+if (!container) throw new Error("Root container not found.");
+
+const root = createRoot(container);
+
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,

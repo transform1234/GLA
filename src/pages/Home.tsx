e732511692dc88febb185dae1dfdaf73e12f96e5
sagar takle <sagarkoshti1990@gmail.com>
2024-12-17 17:37:45 +0530

    Update Grade, Medium, Board, Subject, and Program in CData and Tags Telemetry and odia subject icon (#53)
    * fix: add grade, medium, board, subject, and program in cdata and tags telemetry
    
    * fix:odia subject icon update.
    
    ---------
    
    Co-authored-by: sagar <sagarkoshti1990@gmila.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,6 +11,7 @@ import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import english from "../assets/icons/english_icon.svg";
 import kannada from "../assets/icons/kannada_icon.svg";
+import odia from "../assets/icons/odia_icon.svg";
 import math from "../assets/icons/maths_icon.svg";
 import physics from "../assets/icons/physics_icon.svg";
 import Layout from "../components/common/layout/layout";
@@ -52,6 +53,7 @@ const subjectIcons = {
   math: { icon: math, label: "Math" },
   english: { icon: english, label: "English" },
   kannada: { icon: kannada, label: "Kannada" },
+  odia: { icon: odia, label: "Odia" },
 };
 export default function Homepage() {
   const { t } = useTranslation();
@@ -81,7 +83,6 @@ export default function Homepage() {
     setSelectedSubject(subject);
     localStorage.setItem("subject", subject);
   };
-
 
   return (
     <Layout>

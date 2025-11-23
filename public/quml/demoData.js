1e42ae84e81ed959bdeeb7588e4997af5bf30bbc
sagar <sagarkoshti1990@gmila.com>
2025-11-23 14:53:52 +0530

    fix:quml loding issue

--- public/quml/demoData.js
+++ public/quml/demoData.js
@@ -276,7 +276,22 @@ const playerConfig = {
     userData: metadata?.userData ? metadata?.userData : {},
     cdata: metadata?.playerContext?.cdata || [],
   },
-  metadata: metadata,
+  metadata: {
+    ...metadata,
+    showFeedback: metadata.showFeedback ? "Yes" : "No",
+    showTimer:
+      metadata.showTimer === "Yes" || metadata.showTimer === true
+        ? "Yes"
+        : "No",
+    showSolutions:
+      metadata.showSolutions === "Yes" || metadata.showSolutions === true
+        ? "Yes"
+        : "No",
+    showHints:
+      metadata.showHints === "Yes" || metadata.showHints === true
+        ? "Yes"
+        : "No",
+  },
   config: {
     traceId: "1234",
     sideMenu: {
